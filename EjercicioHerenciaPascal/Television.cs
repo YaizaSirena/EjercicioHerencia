c9393b7d658327d0c341d95ee8a53ae6197f8268
YaizaSirena <yaizamartos@hotmail.com>
2021-03-19 19:41:54 +0100

    Clase Television Creada

---
+++ EjercicioHerenciaPascal/Television.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioHerenciaPascal
+{
+    class Television : Electrodomestico
+    {
+        public double Resolucion { get; set; }
+        public bool TDT { get; set; }
+
+        public Television() : base()
+        {
+            Resolucion = 20;
+            TDT = false;
+        }
+
+        public Television(int precioPa, int pesoPa) : base(precioPa, pesoPa)
+        {
+            Resolucion = 20;
+            TDT = false;
+        }
+        public Television(double resolucionPa, bool TDTPa, int cargaPa, int precioPa, int pesoPa, string consumoPa, string colorPa)
+          : base(precioPa, colorPa, consumoPa, pesoPa)
+        {
+            Resolucion = resolucionPa;
+            TDT = TDTPa;
+        }
+
+        public override void precioFinal() 
+        {
+            if( Resolucion > 40)
+            {
+                PrecioBase *= 1.3;
+            }
+            if( TDT)
+            {
+                PrecioBase += 50;
+            }
+        }
+
+
+    }
+}
